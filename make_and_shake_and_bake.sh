3f38d9cd9369d87d77cc3f6a95a0e06e6555dfa3
Recessive <aa.mindustry@gmail.com>
2021-02-12 17:55:06 +1100

    Updated server version

--- make_and_shake_and_bake.sh
+++ make_and_shake_and_bake.sh
@@ -1,5 +1,5 @@
 export JAVA_HOME=/home/sean/.jdks/openjdk-15.0.1
-./gradlew server:dist -Pbuildversion=122
-./gradlew desktop:dist -Pbuildversion=122
+./gradlew server:dist -Pbuildversion=124
+./gradlew desktop:dist -Pbuildversion=124
 cd server/build/libs
 cp ./server-release.jar ~/Documents/mindustry/V6/servers/assim/server.jar
